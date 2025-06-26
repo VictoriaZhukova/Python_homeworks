71cc8e504108e7fb0aaa003264f687e6dab97510
VictoriaZhukova <zhukova.victoria93@gmail.com>
2025-06-26 16:17:32 +0700

    commit lesson 1

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,4 +1,3 @@
 first_name = input("Ваше имя?")
 last_name = input("Ваша фамилия?")
-space = " "
-print("Вас зовут:" + last_name + space + first_name)
+print("Вас зовут:" + last_name + " " + first_name)
