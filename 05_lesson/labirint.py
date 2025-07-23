158be467af6b4cf1a9dcc515e6126342c8d51b63
VictoriaZhukova <zhukova.victoria93@gmail.com>
2025-07-23 15:12:38 +0700

    коммит 5 урока

---
+++ 05_lesson/labirint.py
@@ -0,0 +1,21 @@
+from time import sleep 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(
+service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("https://labirint.ru/")
+
+search_locator = "#search-field"
+search_input = driver.find_element(By.CSS_SELECTOR, search_locator)
+search_input.send_keys("Python", Keys.RETURN)
+
+books = driver.find_elements(By.CSS_SELECTOR, "div.product-card")
+
+print(len(books))
+
+sleep(5)
