f8c6fcf6288ab331c4749cbd7a725d91902795a1
VictoriaZhukova <zhukova.victoria93@gmail.com>
2025-08-28 17:29:16 +0700

    коммит 8 урока

---
+++ 08_lesson/config.py
@@ -0,0 +1,6 @@
+BASE_URL = "https://ru.yougile.com/api-v2"
+API_TOKEN = "ТОКЕН"  # подставить токен сюда
+HEADERS = {
+    "Authorization": f"Bearer {API_TOKEN}",
+    "Content-Type": "application/json"
+}
